e61947d14bfdf551a16b19025fdd88fbf066969e
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-05-17 18:17:57 +0400

    fixed double main

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,6 @@
 const Home = () => {
   return (
-    <main>
-      <h1 className="text-primary text-center text-3xl">Coming soon</h1>
-    </main>
+    <h1 className="text-primary text-center text-3xl">Coming soon</h1>
   );
 };
 
