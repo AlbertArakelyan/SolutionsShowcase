b9196918e164c528742885967e9f849b75c0d002
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-05-17 08:33:17 +0400

    more steups

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,9 @@
-export default function Home() {
+const Home = () => {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
     </main>
   );
-}
+};
+
+export default Home;
