609df020f4163312ccaae820544b5ef579eb3292
Albert Arakelyan <albertarakelyan1998@gmail.com>
2024-05-29 06:19:22 +0400

    changed main page meta info and added favicon

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,4 +1,23 @@
+import { Metadata } from 'next';
+
 import Solutions from '@/components/pages/Home/Solutions';
+
+import { defaultOpenGraph, mainTitle } from '@/constants/seo';
+
+const title = `Albert Arakelyan | ${mainTitle}`;
+const canonical = '/';
+
+export const metadata: Metadata = {
+  title,
+  alternates: {
+    canonical: canonical,
+  },
+  openGraph: {
+    ...defaultOpenGraph,
+    title,
+    url: canonical,
+  },
+};
 
 const Home = () => {
   return (
